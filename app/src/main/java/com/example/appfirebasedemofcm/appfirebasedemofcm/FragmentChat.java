909b7e769181a5ab8cee8f3adf6007121fa63c55
NguyenThien2297 <thiennguyenvan.kyanon@gmail.com>
2018-12-10 01:20:31 +0700

    pull code

--- app/src/main/java/com/example/appfirebasedemofcm/appfirebasedemofcm/FragmentChat.java
+++ app/src/main/java/com/example/appfirebasedemofcm/appfirebasedemofcm/FragmentChat.java
@@ -6,28 +6,65 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.appfirebasedemofcm.appfirebasedemofcm.Models.Message;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class FragmentChat extends Fragment {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private  RecyclerView.LayoutManager mLayoutManager;
+    private EditText metText;
+    private Button mbtSent;
     private View rootView;
-    String[] myDataset = new String[]{"aaaaaaaaaaa", "b", "c", "d", "e"};
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.chat_fragment,container,false);
         // 1. get a reference to recyclerView
+        mbtSent = (Button) rootView.findViewById(R.id.button_chatbox_send);
+        metText = (EditText)rootView.findViewById(R.id.edittext_chatbox);
 
-        mLayoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.reyclerview_message_list);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-//        mAdapter = new MessageListAdapter(myDataset);
-//        mRecyclerView.setAdapter(mAdapter);
+        mbtSent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = metText.getText().toString();
+                Calendar calendar = Calendar.getInstance();
+                SimpleDateFormat mdformat = new SimpleDateFormat("HH:mm:ss");
+                String strDate = "Current Time : " + mdformat.format(calendar.getTime());
+                if (content.length() > 0) {
+                    metText.setText("");
+                    Message newMessage = new Message(content, "thien",strDate);
+                    FirebaseDatabase.getInstance().getReference().child("message/").push().setValue(newMessage);
+                }
+
+//                if(!TextUtils.isEmpty(content))
+//                {
+//                    // Add a new sent message to the list.
+//                    ChatAppMsgDTO msgDto = new ChatAppMsgDTO(ChatAppMsgDTO.MSG_TYPE_SENT, content);
+//                    msgDtoList.add(msgDto);
+//
+//                    int newMsgPosition = msgDtoList.size() - 1;
+//
+//                    // Notify recycler view insert one new data.
+//                    ListMessageAdapter messageAdapter = new ListMessageAdapter();
+//
+//
+//                    // Scroll RecyclerView to the last message.
+//                    msgRecyclerView.scrollToPosition(newMsgPosition);
+//
+//                    // Empty the input edit text box.
+//
+//                }
+            }
+        });
         return rootView;
     }
 
--- app/src/main/java/com/example/appfirebasedemofcm/appfirebasedemofcm/Models/Message.java
+++ app/src/main/java/com/example/appfirebasedemofcm/appfirebasedemofcm/Models/Message.java
@@ -2,6 +2,41 @@ package com.example.appfirebasedemofcm.appfirebasedemofcm.Models;
 
 public class Message {
     String message;
-    User sender;
-    long createAt;
+    String sender;
+    String createAt;
+
+    public Message(){
+
+    }
+
+    public Message(String _message, String _sender,  String _CreateAt)
+    {
+        this.message = _message;
+        this.sender = _sender;
+        this.createAt = _CreateAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
 }
